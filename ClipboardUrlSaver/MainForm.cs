a92d8b0377ed6eb713bdd061771f91db6030f8ad
Victor L. Senior <publicdomainvic@gmail.com>
2020-05-08 19:10:55 -0400

    Add OnExitToolStripMenuItemClick() code

--- ClipboardUrlSaver/MainForm.cs
+++ ClipboardUrlSaver/MainForm.cs
@@ -319,7 +319,8 @@ namespace ClipboardUrlSaver
         /// <param name="e">Event arguments.</param>
         private void OnExitToolStripMenuItemClick(object sender, EventArgs e)
         {
-            // TODO Add code
+            // Close application
+            this.Close();
         }
 
         /// <summary>
